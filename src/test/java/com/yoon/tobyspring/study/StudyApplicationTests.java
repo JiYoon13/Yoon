640584f285db8236e61d0d48ad304973b05d89b8
JiYoon13 <wldbs347@naver.com>
2024-03-06 23:49:45 +0900

    init

---
+++ src/test/java/com/yoon/tobyspring/study/StudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yoon.tobyspring.study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
